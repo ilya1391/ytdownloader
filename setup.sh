704b358ebf0b10ca66d9daecc474c540f9aaf818
ilya1391 <65591144+ilya1391@users.noreply.github.com>
2021-01-01 15:01:35 +0300

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,1 +1,1 @@
-termux-setup-storage && apt install python -y && pip install --upgrade pip -y && pip install pafy youtube-dll -y && cp -r ytdownloader /sdcard && rm /sdcard/ytdownloader/setup.sh && rm /sdcard/ytdownloader/yt.sh && cp ~/ytdownloader/yt.sh ~/yt.sh && chmod +x ~/yt.sh
+termux-setup-storage && apt install python -y && pip install --upgrade pip && pip install pafy youtube-dll && cp -r ytdownloader /sdcard && rm /sdcard/ytdownloader/setup.sh && rm /sdcard/ytdownloader/yt.sh && cp ~/ytdownloader/yt.sh ~/yt.sh && chmod +x ~/yt.sh

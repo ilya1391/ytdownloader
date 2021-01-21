5d73fbd08c0140deb6eafaacf9b95cb7f5bdadc1
ilya1391 <65591144+ilya1391@users.noreply.github.com>
2021-01-21 13:42:12 +0300

    Delete setup.sh

--- setup.sh
+++
@@ -1,1 +0,0 @@
-termux-setup-storage && apt install python -y && pip install --upgrade pip && pip install pafy youtube-dl && cp -r ytdownloader /sdcard && rm /sdcard/ytdownloader/setup.sh && rm /sdcard/ytdownloader/yt.sh && cp ~/ytdownloader/yt.sh ~/yt.sh && chmod +x ~/yt.sh && clear && echo Установка завершена. Для запуска введите команду ./yt.sh
